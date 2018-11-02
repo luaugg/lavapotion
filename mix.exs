ff11be1025f0efca3f84c9dd44c6a5b699ce46b2
Sam Pritchard <ophisbonklol@gmail.com>
2018-11-02 20:33:38 +0000

    mix file

---
+++ mix.exs
@@ -0,0 +1,32 @@
+defmodule LavaPotion.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :lavapotion,
+      version: "0.1.0",
+      elixir: "~> 1.7",
+      start_permanent: Mix.env() == :prod,
+      deps: deps(),
+      description: "Abstract Lavalink Client for Elixir.",
+      name: "lavapotion",
+      source_url: "https://github.com/SamOphis/lavapotion",
+      package: [
+        licenses: ["Apache 2.0"],
+        links: %{"GitHub" => "https://github.com/SamOphis/lavapotion"}
+      ]
+    ]
+  end
+
+  def application do
+    [extra_applications: [:logger]]
+  end
+
+  defp deps do
+    [
+      {:poison, "~> 3.1"},
+      {:httpoison, "~> 1.4"},
+      {:websockex, "~> 0.4.0"}
+    ]
+  end
+end
