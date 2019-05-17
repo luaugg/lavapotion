6c953463b9149092c0934706e88c8902d5cb1b27
Sam Pritchard <ophisbonklol@gmail.com>
2019-05-17 17:17:39 +0100

    client new function(s)

--- lib/lavapotion/struct/client.ex
+++ lib/lavapotion/struct/client.ex
@@ -1,9 +1,11 @@
 defmodule LavaPotion.Struct.Client do
   defstruct [:user_id, :num_shards]
 
-  def new(opts) do
-    user_id = opts[:user_id]
-    num_shards = opts[:num_shards]
+  def new!([user_id: user_id, num_shards: num_shards]) when is_binary(user_id) and is_number(num_shards) do
+    %__MODULE__{user_id: user_id, num_shards: num_shards}
+  end
+
+  def new([user_id: user_id, num_shards: num_shards]) do
     cond do
       not is_binary(user_id) ->
         {:error, :user_id}
