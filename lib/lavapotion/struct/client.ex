7a07e25370bca71ef41aba1de90918262d7bee38
Sam Pritchard <ophisbonklol@gmail.com>
2019-05-18 14:19:08 +0100

    fixes + typespecs

--- lib/lavapotion/struct/client.ex
+++ lib/lavapotion/struct/client.ex
@@ -1,10 +1,16 @@
 defmodule LavaPotion.Struct.Client do
   defstruct [:user_id, :num_shards]
 
+  @type t :: %__MODULE__{}
+  @type new_client_error :: {:error, :user_id} | {:error, :num_shards}
+  @type client_option :: {:user_id, String.t()} | {:num_shards, integer()}
+
+  @spec new!([client_option]) :: t
   def new!([user_id: user_id, num_shards: num_shards]) when is_binary(user_id) and is_number(num_shards) do
     %__MODULE__{user_id: user_id, num_shards: num_shards}
   end
 
+  @spec new([client_option]) :: {:ok, t} | new_client_error
   def new([user_id: user_id, num_shards: num_shards]) do
     cond do
       not is_binary(user_id) ->
--- lib/lavapotion/struct/node.ex
+++ lib/lavapotion/struct/node.ex
@@ -7,14 +7,20 @@ defmodule LavaPotion.Struct.Node do
 
   defstruct [:authorization, :port, :host, :client, :absolute]
 
+  @type t :: %__MODULE__{}
+  @type node_option :: {:client, Client.t()} | {:host, String.t()} | {:authorization, String.t()} | {:absolute, boolean()}
+  @type new_node_error :: {:error, :client} | {:error, :host} | {:error, :authorization} | {:error, :absolute} | {:error, :port}
+
+  @spec new!([node_option]) :: t
   def new!([client: client = %Client{}, host: host, port: port, authorization: authorization, absolute: absolute])
       when is_binary(host) and is_number(port) and is_binary(authorization) and is_boolean(absolute) do
     %__MODULE__{client: client, port: port, host: host, authorization: authorization, absolute: absolute}
   end
 
+  @spec new([node_option]) :: {:ok, t} | new_node_error
   def new([client: client, host: host, port: port, authorization: authorization, absolute: absolute]) do
     cond do
-      not Map.get(client, :__struct__) == Client ->
+      not (Map.get(client, :__struct__) == Client) ->
         {:error, :client}
       not is_binary(host) ->
         {:error, :host}
@@ -29,20 +35,29 @@ defmodule LavaPotion.Struct.Node do
     end
   end
 
-  def start_link(mod = %__MODULE__{host: host, absolute: true}), do: start_link_with_url(mod, host)
-  def start_link(mod = %__MODULE__{host: host, port: port}), do: start_link_with_url(mod, "ws://#{host}:#{port}")
+  @spec start_link(t) :: {:ok, pid()} | {:error, term()}
+  def start_link(mod = %__MODULE__{host: host, absolute: true}) do
+    start_link_with_url(mod, host)
+  end
 
+  @spec start_link(t) :: {:ok, pid()} | {:error, term()}
+  def start_link(mod = %__MODULE__{host: host, port: port}) do
+    start_link_with_url(mod, "ws://#{host}:#{port}")
+  end
+
+  @spec start_link_with_url(t(), String.t()) :: {:ok, pid()} | {:error, term()}
   defp start_link_with_url(mod = %__MODULE__{client: %Client{user_id: user_id, num_shards: num_shards}, authorization: authorization}, url) do
     extra_headers = ["User-Id": user_id, "Authorization": authorization, "Num-Shards": num_shards]
     result = WebSockex.start_link(url, __MODULE__, %{}, extra_headers: extra_headers, handle_initial_conn_failure: true, async: true)
     if elem(result, 0) === :ok do
       {:ok, pid} = result
-      ets_table_name = "#{user_id}-#{host}"
-      if :ets.whereis(ets_table_name) === :undefined do
+      ets_table_name = :"#{user_id}-#{url}"
+      if :ets.whereis(ets_table_name) == :undefined do
         :ets.new(ets_table_name, [:set, :public, :named_table])
       end
-      :ets.insert(ets_table_name, {host, %{pid: pid, node: mod, stats: nil, players: %{}}})
+      :ets.insert(ets_table_name, {url, %{pid: pid, node: mod, stats: nil, players: %{}}})
     end
     result
   end
+
 end
